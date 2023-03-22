d15e16c4cc9bb0c6b50527381e310cfa5ff39d1a
Lino Padilla <lino.padillad@outlook.com>
2023-03-22 00:25:29 -0500

    Creation of lambda role

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+data "aws_iam_policy_document" "lambda_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type = "Service"
+      identifiers = [ "lambda.amazonaws.com" ]
+    }
+
+    actions = [ "sts:AssumeRole" ]
+  }
+}
+
+resource "aws_iam_role" "lambda_iam_role" {
+  name = "lambda_role_guardDuty_alerts"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+}
